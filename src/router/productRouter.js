39e17ff4a95ef15d296d63d853efb60b9c691bf6
Kartik Malviya <87184927+Kartik-Malviya68@users.noreply.github.com>
2024-03-08 19:06:45 +0530

    ggdfgdgdggdfgdg

--- src/router/productRouter.js
+++ src/router/productRouter.js
@@ -4,9 +4,9 @@ import productControllers from "../controllers/productControllers.js";
 const router = Router();
 router.route("/create").post(productControllers.ProductController);
 router.route("/getAllProducts").get(productControllers.getAllProducts);
-// router
-//   .route("/getProductsByFilter")
-//   .get(productControllers.getProductsByFilter);
+router
+  .route("/getProductsByFilter")
+  .get(productControllers.getProductsByFilter);
 router.route("/:id").get(productControllers.getProductById);
 router.route("/del/:id").delete(productControllers.deleteProduct);
 router.route("/update/:id").put(productControllers.updateProduct);
