ddf395eeed091abe53860bda0c5e7881932c9d52
Kartik Malviya <87184927+Kartik-Malviya68@users.noreply.github.com>
2024-03-16 12:18:51 +0530

    ffsdfdfsdfsdfdfsdf

--- server.js
+++ server.js
@@ -27,11 +27,20 @@ async function main() {
     cors({
       origin: "*",
       methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
-      allowedHeaders: ['Content-Type', 'Authorization'],
+      allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true,
     })
   );
 }
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
