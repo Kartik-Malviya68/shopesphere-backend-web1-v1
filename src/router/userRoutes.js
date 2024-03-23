9814178dc2e5e73106f0f0002f68acbfdf1d279e
Kartik Malviya <87184927+Kartik-Malviya68@users.noreply.github.com>
2024-03-23 19:12:25 +0530

    sdfds

--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -5,7 +5,7 @@ import { verifyJWT } from "../middlewares/authMddleware.js";
 import cartControllers from "../controllers/cartControllers.js";
 
 const router = Router();
-router.route("/register").post(verifyJWT, userControllers.registerUser);
+router.route("/register").post( userControllers.registerUser);
 router.route("/getUserData").get(getUserData);
 router.route("/login").post(userControllers.loginUser);
 router.route("/addToCart").post(verifyJWT, cartControllers.addToCart);
@@ -14,7 +14,7 @@ router
   .route("/removeFromCart/:id")
   .delete(verifyJWT, cartControllers.removeFromCart);
 //secure routes
-router.route("/logout").post( userControllers.logoutUser);
+router.route("/logout").post(verifyJWT, userControllers.logoutUser);
 router.route("/refreshAccessToken").post(userControllers.refreshAccessToken);
 router.route("/checkTokenExpiry").post(verifyJWT, userControllers.checkTokenExpiry);
 export default router;
